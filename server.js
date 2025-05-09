58a8ccd9e4355aec15f9f9c0007309095f2ac088
oriolpujadas <oriol.pujadasmontane@gmail.com>
2025-05-09 18:13:48 +0200

    updates y organizado

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const emailRoutes = require('./routes/emailRoutes');
+const ofertasRoutes = require('./routes/ofertasRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -13,6 +14,7 @@ app.use(express.json());
 // Rutas
 app.use('/api/auth', authRoutes);
 app.use('/api/auth', emailRoutes);
+app.use('/api/auth', ofertasRoutes);
 
 // Iniciar servidor
 app.listen(PORT, () => {
